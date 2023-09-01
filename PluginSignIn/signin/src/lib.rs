d624050b8d2dc1e8b3fb290e6c203bcec323a803
kouqurong <koqurong@outlook.com>
2023-09-01 20:56:34 +0800

    优化代码

--- PluginSignIn/signin/src/lib.rs
+++ PluginSignIn/signin/src/lib.rs
@@ -1,5 +1,6 @@
 use jni::JNIEnv;
-use jni::objects::JObject;
+use jni::objects::{JObject, JString};
+use jni::sys::jint;
 
 extern "Rust"{
   fn api_version() -> String;
@@ -8,7 +9,7 @@ extern "Rust"{
     ///*  `number` 工号
     ///* `token` 令牌
     ///* `bmp_path` 签到图片路径
-    fn api_signin(number: String, token: String, bmp_path: String) -> Result<(), &'static str>;
+   fn api_signin(number: usize, token: String, bmp_path: String) -> Result<String,String>;
 }
 
 
@@ -18,6 +19,23 @@ pub extern "system" fn Java_SignInViewModel_version<'a>(env:JNIEnv<'a>,_obj:JObj
     let version = env.new_string("1.0.0").expect("Couldn't create java string!");
 
    JObject::from(version)
+}
+
+#[no_mangle]
+pub extern "system" fn Java_SignInViewModel_signin<'a>(mut env: JNIEnv<'a>, _obj: JObject, number: jint, token: JString, bmp_path: JString) -> JObject<'a>  {
+    let token: String = env.get_string(&token).expect("Couldn't get java string!").into();
+    let bmp_path: String = env.get_string(&bmp_path).expect("Couldn't get java string!").into();
+
+    let result = unsafe {
+        api_signin(number as usize, token, bmp_path)
+    };
+
+    let result = match result {
+        Ok(s) => env.new_string(s).expect("Couldn't create java string!"),
+        Err(e) => env.new_string(e).expect("Couldn't create java string!"),
+    };
+
+    JObject::from(result)
 }
 
 
@@ -30,11 +48,4 @@ mod test{
         let version = unsafe { api_version() };
         assert_eq!(version, "V1.0.0");
     }
-
-    #[test]
-    fn test_api_signin() {
-        let sigin = unsafe { api_signin("".to_string(), "".to_string(), "".to_string()) };
-        assert_eq!(sigin, Err("工号不能为空"));
-    }
-
 }
