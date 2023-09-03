086fbc0b778f117054993c96deca322106da4e5c
kouqurong <koqurong@outlook.com>
2023-09-03 13:43:41 +0800

    修复包名问题

--- PluginSignIn/signin/src/lib.rs
+++ PluginSignIn/signin/src/lib.rs
@@ -15,14 +15,14 @@ extern "Rust"{
 
 
 #[no_mangle]
-pub extern "system" fn Java_SignInViewModel_version<'a>(env:JNIEnv<'a>,_obj:JObject<'a>) -> JObject<'a> {
+pub extern "system" fn Java_com_kouqurong_plugin_signin_SignInViewModel_version<'a>(env:JNIEnv<'a>,_obj:JObject<'a>) -> JObject<'a> {
     let version = env.new_string("1.0.0").expect("Couldn't create java string!");
 
    JObject::from(version)
 }
 
 #[no_mangle]
-pub extern "system" fn Java_SignInViewModel_signin<'a>(mut env: JNIEnv<'a>, _obj: JObject, number: jint, token: JString, bmp_path: JString) -> JObject<'a>  {
+pub extern "system" fn Java_com_kouqurong_plugin_signin_SignInViewModel_signin<'a>(mut env: JNIEnv<'a>, _obj: JObject, number: jint, token: JString, bmp_path: JString) -> JObject<'a>  {
     let token: String = env.get_string(&token).expect("Couldn't get java string!").into();
     let bmp_path: String = env.get_string(&bmp_path).expect("Couldn't get java string!").into();
 
